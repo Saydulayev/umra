e49a25e68078f973d14db1c35e0144ea6bfc8d13
Ahmad <marsho13@gmail.com>
2023-01-26 12:46:20 +0100

    Initial Commit

---
+++ umra/umraApp.swift
@@ -0,0 +1,17 @@
+//
+//  umraApp.swift
+//  umra
+//
+//  Created by Dokx Dig on 26.01.23.
+//
+
+import SwiftUI
+
+@main
+struct umraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
