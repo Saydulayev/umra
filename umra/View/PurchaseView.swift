2e80a36d1a3719bf4b457e38c314ab20249a25ed
Saydulayev <saydulayev.wien@gmail.com>
2023-12-08 17:37:53 +0100

    Refactor StoreVM and Views for Donation Functionality
    - Removed RenewalInfo and RenewalState typealias, focusing on consumable products for donations.
    - Updated PurchaseView to remove references to subscriptionGroupStatus, aligning with the donation context.
    - Ensured StoreVM handles consumable product transactions without subscription renewal logic.
    - Streamlined DonationSheetView for better user experience with dynamic product price display.
    - Improved overall code structure for clearer, more focused functionality on donations.

--- umra/View/PurchaseView.swift
+++ umra/View/PurchaseView.swift
@@ -11,19 +11,13 @@ import StoreKit
 
 struct PurchaseView: View {
     @StateObject var storeVM = StoreVM()
+
     var body: some View {
         VStack {
-            if let subscriptionGroupStatus = storeVM.subscriptionGroupStatus {
-                if subscriptionGroupStatus == .expired || subscriptionGroupStatus == .revoked {
-                    Text("Welcome back, give the donation another try.")
-                    //display products
-                }
-            }
             if storeVM.purchasedSubscriptions.isEmpty {
                 SubscriptionView()
-                
             } else {
-                Text("Premium Content")
+                Text("Thank you for your support!")
             }
         }
         .environmentObject(storeVM)
