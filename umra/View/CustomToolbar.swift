f45041c4b793a85d9acb9d80da2c1eeb54b81a91
Saydulayev <saydulayev.wien@gmail.com>
2026-03-13 00:05:59 +0100

    feat: добавление локализации для выбора шрифта в CustomToolbar
    - Добавлена поддержка локализации для текста "Выберите шрифт" в `CustomToolbar`.
    - Обновлены файлы локализации для нескольких языков, включая арабский, немецкий, английский, французский, индонезийский, русский и турецкий, с добавлением соответствующих переводов.

--- umra/View/CustomToolbar.swift
+++ umra/View/CustomToolbar.swift
@@ -13,20 +13,26 @@ import Foundation
 struct CustomToolbar: View {
     @Binding var selectedFont: String
     @Environment(ThemeManager.self) private var themeManager
+    @Environment(LocalizationManager.self) private var localizationManager
     var fonts: [String]
 
     var body: some View {
         HStack {
             Menu {
-                Picker(selection: $selectedFont, label: Text("Выберите шрифт")) {
+                Picker(selection: $selectedFont, label: Text("select_font", bundle: localizationManager.bundle)) {
                     ForEach(fonts, id: \.self) { font in
                         Text(font).tag(font)
                     }
                 }
             } label: {
-                Image(systemName: "textformat").imageScale(.large).foregroundStyle(.primary)
+                Label {
+                    Text("select_font", bundle: localizationManager.bundle)
+                } icon: {
+                    Image(systemName: "textformat")
+                }
+                .imageScale(.large)
+                .foregroundStyle(.primary)
             }
-            .accessibilityLabel("Select Font")
         }
     }
 }
